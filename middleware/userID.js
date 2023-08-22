ebc96c35baf413d0641e9025688d18514c40e64e
timabs <timur.abd.nyc@gmail.com>
2023-08-22 00:15:41 -0400

    main

--- middleware/userID.js
+++ middleware/userID.js
@@ -2,7 +2,13 @@ const { v4: generateNewUserId } = require("uuid");
 
 const uIDMiddleware = (req, res, next) => {
   const userId = req.cookies.userId || generateNewUserId();
-  res.cookie("userId", userId, { maxAge: 30 * 24 * 60 * 60 * 1000 }); // Cookie lasts for 30 days
+  res.cookie("userId", userId, {
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    domain: "https://stickynotes.lol/",
+    secure: true,
+    httpOnly: true,
+    sameSite: "none",
+  }); // Cookie lasts for 30 days
   req.userId = userId;
   next();
 };
